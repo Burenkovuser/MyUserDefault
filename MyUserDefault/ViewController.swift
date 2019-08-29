f3fdb14463ab02c439c2380eb454f289293a80c6
Burenkovuser <nb2207@yandex.ru>
2019-08-29 17:12:27 +0700

    1. Done button 2. Save to user defaults 3 Default user defaults

--- MyUserDefault/ViewController.swift
+++ MyUserDefault/ViewController.swift
@@ -9,12 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var secondNameTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        userNameLabel.isHidden = true
+        
+        //получаем сохраненные данные
+        if let user = UserDefaults.standard.value(forKey: "userName") {
+            userNameLabel.isHidden = false
+            userNameLabel.text = user as? String
+        }
     }
-
-
+    
+    @IBAction func donProcessed() {
+        
+        //проверяем что данные не пустые
+        guard let fistName = firstNameTextField.text, !(fistName.isEmpty) else {
+            wrongFormatAlert()
+            return
+        }
+        guard let secondName = secondNameTextField.text, !(secondName.isEmpty) else {
+            wrongFormatAlert()
+            return
+        }
+        //проверяем чтобы не использовались числа
+        if let _ = Double(fistName) {
+            wrongFormatAlert()
+        } else if let _ = Double(secondName) {
+            wrongFormatAlert()
+        } else {
+            userNameLabel.isHidden = false
+            userNameLabel.text = fistName + " " + secondName
+            UserDefaults.standard.set(userNameLabel.text, forKey: "userName")//сохраняем значение
+        }
+        
+        //стираем данные после нажатия кнопки
+        firstNameTextField.text = nil
+        secondNameTextField.text = nil
+    }
+    
 }
 
+extension ViewController {
+    
+    private func wrongFormatAlert() {
+        
+        let alert = UIAlertController(
+            title: "Wrong Format!",
+            message: "Please enter your name",
+            preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+}
